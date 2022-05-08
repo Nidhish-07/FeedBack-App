cba0c1baeb495d00c96af4d5182cfbb5adbd67fc
Nidhish Chauhan <rch246chauhan@gmail.com>
2022-05-08 18:07:14 +0530

    Commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,30 @@
 <script>
-import FeedbackList from "./components/FeedbackList.svelte";
+  import FeedbackList from "./components/FeedbackList.svelte";
 
-let feedback=[
-  {
-    id: 1,
-    rating: 10,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-  },
-  {
-    id: 2,
-    rating: 9,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-  },
-  {
-    id: 3,
-    rating: 8,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-  },
-]
+  let feedback = [
+    {
+      id: 1,
+      rating: 10,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+    },
+  ];
+
+  const deleteFeedback=(event)=>{
+    const itemID=event.detail;
+    feedback=feedback.filter((item)=>item.id!=itemID)
+  }
 </script>
 
-<main>
-	<FeedbackList {feedback} />
+<main class="container">
+  <FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
 </main>
-
